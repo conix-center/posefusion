e409eb8ca64941426d89ce25ac4ea8a52af9ce2a
wiselab <ekli@andrew.cmu.edu>
2019-07-30 14:39:35 -0400

    image grabber for calibration

---
+++ image-grab.cpp
@@ -0,0 +1,29 @@
+#include <openpose/headers.hpp>
+
+using namespace cv;
+using namespace std;
+
+const string LAMBDA_NUMBER = "1";
+const string FILEPATH = "./media/calib" + LAMBDA_NUMBER + ".jpg";
+
+int main(int argc, char const *argv[])
+{
+	VideoCapture cam(0);
+	if (!cam.isOpened())
+	{
+		cerr << "No video cam found." << endl;
+		return 1;
+	}
+
+	Mat im;
+
+	while (waitKey(1) != 'q')
+	{ 	
+		cam >> im;
+		imshow("image", im);
+	}
+
+	imwrite(FILEPATH, im);
+
+	return 0;
+}
