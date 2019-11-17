bc648800bcffe7fb1a63771c316176ea8d4ed715
CorentinDugue <corentindugue@yahoo.fr>
2019-11-17 17:34:07 -0500

    posefusion: comment functions

--- python/posefusion.py
+++ python/posefusion.py
@@ -19,25 +19,36 @@ TOPIC_POSE      = "/lambda/+/pose"
 TOPIC_CAMERA    = "/posefusion/camera/"
 CLIENT_ID       = "posefusion"
 MATRICES_PATH   = "matrices.xml"
-PROJS_PATH      = "../results/projections.npz"
+PROJS_PATH      = "../../results/projections.npz"
+INTRINSICS_PATH = "calib1.npz"
 
 # Global parameters
+# Configuation
 MAX_NUM_PEOPLE  = 20
-ERR_THRESHOLD   = 10000
-REF_CAM         = 2
 NUM_CAMERAS     = 3
-FRAME_AVERAGING = 1
-MIN_BODY_CALIB  = 500
-MIN_BODY_POINTS = 50
+REF_CAM         = 2
 CONF_SCORE      = 0.7
+
+# Calibration
 LOAD_PROJS      = True
+MIN_BODY_CALIB  = 500
+
+# Reconstruction
+ERR_THRESHOLD   = 10000
+FRAME_AVERAGING = 1
+MIN_BODY_POINTS = 50
 
 # Globals
 running = False
 received_data = [False, False, False]
 calib_done = False
 
-# Read projection matrices from matrices.xml into a (3,4,NUMCAMERAS) numpy array
+'''
+read_matrices: Read projection matrices from the specified file path in a numpy array of 
+               shape (NUM_CAMERAS, 3, 4).
+    Input:  F, fundamental matrix
+    Output: projection_matrices, projection matrices (NUM_CAMERAS, 3, 4)
+'''
 def read_matrices(file_path):
     cv_file = cv2.FileStorage(file_path, cv2.FILE_STORAGE_READ)
     numCameras = int(cv_file.getNode("NumCameras").mat())
@@ -47,28 +58,46 @@ def read_matrices(file_path):
         projection_matrices[camera] = matrix
     return projection_matrices
 
+'''
+repro_error: compute the reprojection error of a computed 3D point.
+    Input:  pts4D, points in 3D in homogeneous coordinates (4th point is 1)
+            proj0, projection matrix of first camera used for triangulation
+            proj1, projection matrix of second camera used for triangulation
+            pt1, point of first camera
+            pt2, point of second camera
+    Output: total, reprojection error
+            sum_per_part, error per body part
+'''
 def repro_error(pts4D, proj0, proj1, pt1, pt2):
     # Obtain estimation of pt1
     pt1_est = np.dot(proj0,pts4D.T).T
+    
     # Obtain 2D coordinates normalized (divide by z, I think)
     pt1_est = pt1_est[:, :2]/np.repeat(pt1_est[:, 2], 2).reshape(-1, 2)
+    
     # Obtain error for each point 
-    # error = np.sqrt(np.power(pt1_est[:,0] - pt1.T[:,0], 2)+np.power(pt1_est[:,1] - pt1.T[:,1], 2))
     error = np.sqrt(np.sum((pt1_est-pt1.T)**2, axis=1))
     
     # Obtain estimation of pt2
     pt2_est = np.dot(proj1,pts4D.T).T
+    
     # Obtain 3D coordinates normalized (divide by z, I think)
     pt2_est = pt2_est[:, :2]/np.repeat(pt2_est[:, 2], 2).reshape(-1, 2)
+    
     # Obtain error for each point 
-    # error2 = np.sqrt(np.power(repro2[:,0] - pt2.T[:,0], 2)+np.power(repro2[:,1] - pt2.T[:,1], 2))
     error2 = np.sqrt(np.sum((pt2_est-pt2.T)**2, axis=1))
     
-    sum_per_part = (error + error2)/2
+    sum_per_part = (error + error2) / 2
     total = np.sum(sum_per_part)
     
     return total, sum_per_part
 
+'''
+distance_points: compute the distance between two points.
+    Input:  p1, first point coordinate (numpy array)
+            p2, second point coordinate (numpy array)
+    Output: dist, distance between the two points provided
+'''
 def distance_points(p1, p2):
     squared_dist = np.sum((p2-p1)**2, axis = 0)
     dist = np.sqrt(squared_dist)
@@ -76,6 +105,13 @@ def distance_points(p1, p2):
 
 # INTERPOLATE PARTS OF THE BODY IF POSSIBLE!!
 
+'''
+fusion_body: merge two bodies. If the body part of the new body is non zero then the fusioned body
+             used two coordinates, otherwise it uses the previous body coordinates for that part.
+    Input:  prev_body, body points of previous body
+            new_body, body points of new body
+    Output: fusioned_body, body points resulting from the mix of previous and new body
+'''
 def fusion_body(prev_body, new_body):
     fusioned_body = np.empty((25,3))
     for i in range(prev_body.shape[0]):
@@ -85,8 +121,12 @@ def fusion_body(prev_body, new_body):
             fusioned_body[i] = prev_body[i]
     return fusioned_body
 
+'''
+check_body_length: given a body in 3D it applies several filters to ensure that the overall shape
+                   of the body is valid.
+    Input:  body, body points in 3D
+'''
 def check_body_length(body):
-    
     # Arms
     r_upper_arm = distance_points(body[2], body[3])
     r_lower_arm = distance_points(body[3], body[4])
@@ -94,7 +134,7 @@ def check_body_length(body):
     l_upper_arm = distance_points(body[5], body[6])
     l_lower_arm = distance_points(body[6], body[7])
     l_arm = l_upper_arm + l_lower_arm
-    
+
     # Legs
     r_upper_leg = distance_points(body[9], body[10])
     r_lower_leg = distance_points(body[10], body[11])
@@ -103,7 +143,7 @@ def check_body_length(body):
     l_lower_leg = distance_points(body[13], body[14])
     l_leg = l_upper_leg + l_lower_leg
     
-    # Foots
+    # Feet
     r_foot = distance_points(body[22], body[24])
     l_foot = distance_points(body[19], body[21])
     
@@ -117,12 +157,10 @@ def check_body_length(body):
     dist_ears = distance_points(body[17], body[18])
         
     if (l_head > 0.5):
-        body[17] = 0
-        body[15] = 0
+        body[17] = body[15] = 0
 
     if (r_head > 1):
-        body[18] = 0
-        body[16] = 0
+        body[18] = body[16] = 0
 
     if (dist_neck_l_eye > 1):
         body[15] = 0
@@ -135,67 +173,47 @@ def check_body_length(body):
         
      # Check right arm lengths
     if ((r_arm > 1) or (r_arm <= 0)):
-        # print("Right arm length incorrect")
-        body[2] = 0
-        body[3] = 0
-        body[4] = 0
+        body[2] = body[3] = body[4] = 0
     
     # Check arm lengths
     if ((l_arm > 1) or (l_arm <= 0)):
-        # print("Left arm length incorrect")
-        body[5] = 0
-        body[6] = 0
-        body[7] = 0
+        body[5] = body[6] = body[7] = 0
 
     # Check left leg lengths
     if ((l_upper_leg > 1) or (l_upper_leg <= 0)):
-        # print("Left upper leg length incorrect")
-        body[9] = 0
-        body[10] = 0
+        body[9] = body[10] = 0
     
     if ((l_lower_leg > 1) or (l_lower_leg <= 0)):
-        # print("Left lower leg length incorrect")
-        body[10] = 0
-        body[11] = 0
+        body[10] = body[11] = 0
              
     # Check right leg lengths
     if ((r_upper_leg > 1) or (r_upper_leg <= 0)):
-        # print("Right upper leg length incorrect")
-        body[12] = 0
-        body[13] = 0
+        body[12] = body[13] = 0
     if ((r_lower_leg > 1) or (r_lower_leg <= 0)):
-        # print("Right lower leg length incorrect")
-        body[13] = 0
-        body[14] = 0
+        body[13] = body[14] = 0
 
-    # print("{:.2f} {:.2f}".format(r_foot, l_foot))
-       
     # Check foot length
     if (r_foot > 0.5) or (r_foot <= 0):
-        # print("Right foot length incorrect")       
-        body[22] = 0
-        body[24] = 0
-        body[11] = 0
-        body[23] = 0
+        body[22] = body[24] = body[11] = body[23] = 0
         
     if (l_foot > 0.5) or (l_foot <= 0):
-        # print("Left foot length incorrect")       
-        body[14] = 0
-        body[19] = 0
-        body[20] = 0
-        body[21] = 0
-    
+        body[14] = body[19] = body[20] = body[21] = 0
+
     return True
 
+'''
+filter_body: apply different filters to the body
+    Input:  body, body points in 3D
+'''
 def filter_body(body):
     # Body length
     check_body_length(body)
     
     # Max greater than 10
-    body[body > 2] = 0
+    body[body > 10] = 0
     
-    # Min less than -2
-    body[body < -2] = 0
+    # Min less than -10
+    body[body < -10] = 0
 
     
     if (not np.all(np.isfinite(body))):
@@ -204,7 +222,17 @@ def filter_body(body):
     
     return True
     
-
+'''
+triangulateTwoBodies: obtain 3D reconstruction of body given body coordinates from two different 
+                      cameras.
+    Input:  camera_1, projection matrix of first camera
+            camera_1, projection matrix of second camera
+            pt1, 2D coordinates of body of first camera
+            pt2, 2D coordinates of body of second camera
+    Output: error, overall reprojection error (scalar)
+            error_mat, reprojection error per body part
+            pts3D, 3D coordinates obtained from triangulation
+'''
 def triangulateTwoBodies(camera_1, camera_2, pt1, pt2):
     # Transform into appropriate format for OpenCV
     pt1 = np.vstack((pt1[:,0], pt1[:,1]))
@@ -223,6 +251,14 @@ def triangulateTwoBodies(camera_1, camera_2, pt1, pt2):
     error, error_mat = repro_error(pts4D, camera_1, camera_2, pt1, pt2)
     return error, error_mat, pts3D
 
+'''
+convertTo3DPoints: given a set of 2D coordinates cameras, find best 3D reconstruction.
+    Input:  ref_cam, reference camera used to perform triangulation
+            number_cameras, number of cameras
+    Output: saved3DCoordinates, 3D coordinates of the valid bodies found
+            num_body_ok, number of bodies returned
+            list_cam_chosen, list of the cameras chosen for triangulation
+'''
 def convertTo3DPoints(ref_cam, number_cameras):
     cam_chosen = 0
     body_num_chosen = 0
@@ -269,7 +305,11 @@ def convertTo3DPoints(ref_cam, number_cameras):
         
     return saved3DCoordinates, num_body_ok, list_cam_chosen
 
-
+'''
+get_num_body_camera: helper function to find number of bodies detected by a camera
+    Input:  camera_num, camera of interest
+    Output: body_count, number of bodies found by that camera in that frame
+'''
 def get_num_body_camera(camera_num):
     body_count = 0
     for i in range(dataPoints.shape[3]):
@@ -277,14 +317,21 @@ def get_num_body_camera(camera_num):
             body_count = body_count + 1
     return body_count
 
+'''
+find_new_mapping: find the new mapping of bodies to points
+    Input:  prev, set of previous bodies
+            new, set of new bodies
+            numBodies, number of bodies found
+            prev_mapping, previous mapping
+    Output: mapping, new mapping
+            prev_updated, replacement for the previous body array
+'''
 def find_new_mapping(prev, new, numBodies, prev_mapping):
     # Initialize the new mapping from the previous one
     mapping = prev_mapping.copy()
 
     prev_updated = np.empty((25, 3, MAX_NUM_PEOPLE))
 
-
-    # print(mapping)
     # Create list of bodies
     body_list = list(range(numBodies))
 
@@ -292,8 +339,8 @@ def find_new_mapping(prev, new, numBodies, prev_mapping):
     for person in range(numBodies):
         error = 666666666
         min_b = 0
-        # print(prev_mapping[person])
         prev_ = prev[:,:,prev_mapping[person]]
+
         # Check which of the new body is the closest to the previous one
         for body_num in body_list:
             new_ = new[:,:,body_num]
@@ -312,12 +359,6 @@ def find_new_mapping(prev, new, numBodies, prev_mapping):
         # prev_updated[:,:,min_b] = fusion_body(prev_, new[:,:,min_b])
         prev_updated[:,:,min_b] = new[:,:,min_b]
 
-
-    # Set previous bodies to be new
-    # prev = new
-    # print("upd")
-    # print(prev_updated[:5, :, 0])
-
     return mapping, prev_updated
 
 ######################### MQTT Functions #########################
@@ -327,19 +368,18 @@ def on_connect(client, userdata, flags, rc):
 
 
 def on_message(client, userdata, msg):
-    # Parse data
+    # Obtain lambda ID from message
     lambda_id = int(msg.topic[8])
     
+    # Convert message into string
     rcv_msg = str(msg.payload.decode("utf-8"))
 
-    # print(len(rcv_msg))
-
+    # Frame containing body coordinates
     if (len(rcv_msg) > 50):
 
         people_list = rcv_msg.split(",")
 
         for body in people_list[:-1]:
-            # print(body)
             person_id, arr, ts, score = parse_body_from_mqtt(body)
 
             global calib_done
@@ -353,30 +393,27 @@ def on_message(client, userdata, msg):
             if (len(pts_calib[0]) > MIN_BODY_CALIB):
                 calib_done = True
 
-            # Store data in appropriate array
+            # Store data in appropriate array if the calibration is done and confidence score large enough
             if (calib_done == True):
                 if (score > CONF_SCORE):
-                    # print(person_id, score)
                     dataPoints[lambda_id-1, :, :, int(person_id)] = arr
-
+    # Synchronization frame
     else:
         dataPoints[lambda_id-1].fill(0)
 
-
-        
+    # print("{}, {}, {}".format(ts, lambda_id, person_id))
     received_data[lambda_id - 1] = True
 
-    # print("{}, {}, {}".format(ts, lambda_id, person_id))
 
-
-    # Obtain array
-    # points = np.vstack(pts1_calib)
-
-
-        
-
-
-# Take a string and returns the number id of the person along with an array of its coordinates
+'''
+parse_body_from_mqtt: obtain the id of the person, 2D points, timestamp and confidence score
+                      from part of MQTT message for a person
+    Input:  message, string of the received message
+    Output: person_id, id of the person from OpenPose
+            body_arr, array of 2D points (25,2)
+            ts, timestamp of the frame
+            conf_score, confidence score obtained from OpenPose for that person
+'''
 def parse_body_from_mqtt(message):
     l = message.split()
     body_arr = np.array(l[3:], dtype=float).reshape(25,2)
@@ -385,7 +422,12 @@ def parse_body_from_mqtt(message):
     ts = l[0]
     return person_id, body_arr, ts, conf_score
 
-
+'''
+publish_person: given the ID of a person and its 3D coordinates, prepare an MQTT message.
+    Input:  coord_3D, 3D coordinates to be published
+            person_id, id of the person
+    Output: message, string of the message to be sent
+'''
 # Parse array of 3D coordinates of a person to string to be sent to MQTT
 def publish_person(coord_3D, person_id):
     message = json.dumps(coord_3D.flatten().tolist())
@@ -393,7 +435,11 @@ def publish_person(coord_3D, person_id):
     message = 'Person' + str(person_id) + ',' + message + ",on"
     return message
 
-# Parse array of 3D coordinates of a person to string to be sent to MQTT
+'''
+publish_camera: generate message to be sent through MQTT of the [tx, ty, tz] vector of projection matrix
+    Input:  projection, projection matrix (3, 4)
+    Output: message, string of the message to be sent
+'''
 def publish_camera(projection):
     # zero_coord = np.array([1, 1, 1, 1])    
     zero_coord = np.array([0, 0, 0, 1])      
@@ -423,15 +469,14 @@ if __name__ == '__main__':
         client.loop(0.1)
     print("Collect data finished")
 
-    np.savez("../results/pts_calib.npz", pts_calib0=pts_calib[0],pts_calib1=pts_calib[1],pts_calib2=pts_calib[2], date=int(time.time()))
+    # np.savez("../results/pts_calib.npz", pts_calib0=pts_calib[0],pts_calib1=pts_calib[1],pts_calib2=pts_calib[2], date=int(time.time()))
 
     # Create empty numpy array to hold projection matrices
     projs = np.empty((NUM_CAMERAS, 3, 4))
     m_matrices = np.empty((NUM_CAMERAS, 3, 4))
 
     # Default intrinsic matrices
-    K1 = K2 = np.load("calib1.npz")["mtx"]     
-    # K2 = np.load("calib2.npz")["mtx"]   
+    K1 = K2 = np.load(INTRINSICS_PATH)["mtx"]     
 
     # Load projection matrices
     if (LOAD_PROJS == True):
@@ -445,11 +490,11 @@ if __name__ == '__main__':
     else:
         # Iterate through each pair of camera
         for camera_num in range(NUM_CAMERAS-1):
+            print("Camera{}: {} bodies collected", camera_num, len(pts_calib[camera_num]))
             projs[0], projs[camera_num + 1], m_matrices[0], m_matrices[camera_num + 1] = calibration.get_projs_matrices(pts_calib[0], pts_calib[camera_num + 1], K1, K2)
         # Save resulting projection matrices
-        np.savez("../results/projections.npz", num_cameras = NUM_CAMERAS, K1=K1, K2=K2, projs=projs, m_matrices = m_matrices,date=int(time.time()))
+        np.savez(PROJS_PATH, num_cameras = NUM_CAMERAS, K1=K1, K2=K2, projs=projs, m_matrices = m_matrices,date=int(time.time()))
 
-    # ipdb.set_trace();
     # Read matrices.xml
     # projs = read_matrices(MATRICES_PATH)
 
@@ -468,13 +513,13 @@ if __name__ == '__main__':
         id_mapping[i] = i
 
 
-    print(0, m_matrices[0, :, -1])
-    print(1, m_matrices[1, :, -1])
-    print(2, m_matrices[2, :, -1])
+    # print(0, m_matrices[0, :, -1])
+    # print(1, m_matrices[1, :, -1])
+    # print(2, m_matrices[2, :, -1])
 
-    print(0, projs[0])
-    print(1, projs[1])
-    print(2, projs[2])
+    # print(0, projs[0])
+    # print(1, projs[1])
+    # print(2, projs[2])
 
     # print(1, m_matrices[1, :, -1])
     # print(2, m_matrices[2, :, -1])
@@ -483,30 +528,27 @@ if __name__ == '__main__':
     while True:
         # Triangulate
         if (received_data[0] and received_data[1] and received_data[2]):
-            # print(received_data[0]-received_data[1])
             received_data[0] = received_data[1] = received_data[2] = False
             if (not running):
                 # Make sure we don't run that algorithm conccurently
                 running = True
 
+                # Publish camera coordinates to be displayed in ARENA
                 for i in range(NUM_CAMERAS):
                     client.publish(TOPIC_CAMERA + str(i), publish_camera(m_matrices[i])) 
 
                 # 1. Obtain 3D coordinates of people using triangulation
                 points3D, numBodies, list_cam_chosen = convertTo3DPoints(REF_CAM, NUM_CAMERAS)
-                if (len(list_cam_chosen)):
-                        print(list_cam_chosen)
-
+                # if (len(list_cam_chosen)):
+                        # print(list_cam_chosen)
 
                 # 2. Filter new body that arrived
                 for body_num in range(numBodies):
                     filter_body(points3D[:,:,body_num])
 
                 # 3. Find new mapping of people id
-                # REPLACE prev3DPoints here?????
                 # id_mapping, prev3DPoints = find_new_mapping(prev3DPoints, points3D, numBodies, id_mapping)
                 id_mapping, prev = find_new_mapping(prev3DPoints, points3D, numBodies, id_mapping)
-                
                 
                 # Publish each body separately
                 for person in range(numBodies):                
@@ -555,25 +597,3 @@ if __name__ == '__main__':
 
         # Get new messages
         client.loop(0.01)
-
-
-    # # Data collection
-    # index = ["nose", "neck", "rshoulder", "relbow", "rwirst", "lshoulder", "lelbow", "lwirst", "midhip", "rhip", "rknee", "rankle", "lhip", "lknee", "lankle", "reye", "leye", "rear", "lear", "lbigtoe", "lsmalltoe", "lheel", "rbigtoe", "rsmalltoe", "rheel"]
-    # columns = ["body_num", "x", "y", "z"]
-    # df = pd.DataFrame(index=index, columns=columns)
-    # exported = False
-    # unique_num = 0
-
-#     # Collect data
-# if (exported == False):
-#     unique_num += 1
-#     df2 = pd.DataFrame(index=index, columns=columns)
-#     df2.loc[:, "unique_num"] = unique_num
-#     df2.loc[:, "person_num"] = person
-#     df2.loc[:, "body_num"] = id_mapping[person]
-#     df2.loc[:, ["x", "y", "z"]] = points3D[:,:,id_mapping[person]]
-#     df= df.append(df2)
-#     if (unique_num > 100 and exported == False):
-#         df.to_csv("bodies.csv")
-#         print("EXPORTED TO CSV")
-#         exported = True 
